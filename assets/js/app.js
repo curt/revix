981b1dfa5d1ba07cee3473ce47526664a82a81c0
Curt Gilman <14070+curt@users.noreply.github.com>
2026-03-01 20:28:11 -0700

    remediate premature long-poll connection

--- assets/js/app.js
+++ assets/js/app.js
@@ -78,7 +78,6 @@ Hooks.PlaceSearch = {
 
 const csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 const liveSocket = new LiveSocket("/live", Socket, {
-  longPollFallbackMs: 2500,
   params: {_csrf_token: csrfToken},
   hooks: Hooks,
 })
